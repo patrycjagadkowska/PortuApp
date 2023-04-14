beb47cc073d17c59b7102b2dc71bce01ab3c24e5
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-04-14 15:19:30 +0100

    add forms with styles

--- src/components/AuthForms/CreateAccountForm.js
+++ src/components/AuthForms/CreateAccountForm.js
@@ -1,8 +1,34 @@
-const CreateAccountForm = props => {
-    return (
-        <form>
+import classes from './AuthForm.module.css';
 
-        </form>
+const CreateAccountForm = () => {
+    const submitHandler = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+      <form className={classes.form} onSubmit={submitHandler}>
+        <label className={classes["form__label"]} htmlFor="email">
+          Email
+        </label>
+        <input className={classes["form__input"]} type="email" id="email" />
+        <label className={classes["form__label"]} htmlFor="password">
+          Password
+        </label>
+        <input
+          className={classes["form__input"]}
+          type="password"
+          id="password"
+        />
+        <label className={classes["form__label"]} htmlFor="repeat-password">
+          Repeat password
+        </label>
+        <input
+          className={classes["form__input"]}
+          type="password"
+          id="repeat-password"
+        />
+        <button className={classes["form__submit-btn"]}>Submit</button>
+      </form>
     );
 };
 
--- src/components/AuthForms/LoginForm.js
+++ src/components/AuthForms/LoginForm.js
@@ -1,7 +1,17 @@
-const LoginForm = props => {
-    return (
-        <form>
+import classes from './AuthForm.module.css';
 
+const LoginForm = () => {
+    const submitHandler = (event) => {
+        event.preventDefault();
+    };
+    
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <label className={classes['form__label']} htmlFor='email'>Email</label>
+            <input className={classes['form__input']} type='email' id='email' />
+            <label className={classes['form__label']} htmlFor='password'>Password</label>
+            <input className={classes['form__input']} type='password' id='password' />
+            <button className={classes['form__submit-btn']}>Submit</button>
         </form>
     );
 };
