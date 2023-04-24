40cfe715fe7ac9eee7524130ec4909d766c2063c
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-04-24 12:41:20 +0100

    add conditions to nav, logout method and styles

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 
 import AuthContext from '../../context/AuthContext';
@@ -8,17 +8,38 @@ import classes from './styles/Header.module.css';
 const Header = () => {
     const authCtx = useContext(AuthContext);
     const isLoggedIn = authCtx.isLoggedIn;
+    const navigate = useNavigate();
+
+    const logoutHandler = () => {
+        authCtx.logout();
+        navigate('/');
+    };
 
     return (
-        <header className={classes.header}>
-            <div className={classes['header__logo']}>
-                <NavLink to='/'>PortuApp</NavLink>
-            </div>
-            <nav className={classes['header__nav']}>
-                {!isLoggedIn && <NavLink to='/createAccount'>Start learing</NavLink>}
-                {!isLoggedIn && <NavLink to='/login'>Log in</NavLink>}
-            </nav>
-        </header>
+      <header className={classes.header}>
+        <div className={classes["header__logo"]}>
+          <NavLink to="/">PortuApp</NavLink>
+        </div>
+        <nav className={classes["header__nav"]}>
+          {!isLoggedIn && <NavLink to="/createAccount">Start learing</NavLink>}
+          {!isLoggedIn && <NavLink to="/login">Log in</NavLink>}
+          {isLoggedIn && (
+            <NavLink to="/learn" className={classes.loggedInNavLink}>
+              Learn
+            </NavLink>
+          )}
+          {isLoggedIn && (
+            <NavLink to="/profile" className={classes.loggedInNavLink}>
+              Profile
+            </NavLink>
+          )}
+          {isLoggedIn && (
+            <button onClick={logoutHandler} className={classes.logoutBtn}>
+              Logout
+            </button>
+          )}
+        </nav>
+      </header>
     );
 };
 
