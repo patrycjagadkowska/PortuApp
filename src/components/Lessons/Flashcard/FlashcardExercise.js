66d79e146ae472e5445f13e90b52988900a7024c
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-05-08 15:49:05 +0100

    add validation to flashcard exercise

--- src/components/Lessons/Flashcard/FlashcardExercise.js
+++ src/components/Lessons/Flashcard/FlashcardExercise.js
@@ -2,14 +2,22 @@ import classes from './styles/FlashcardExercise.module.css';
 
 const FlashcardExercise = (props) => {
     const { question, answers, correct } = props.data;
+    const checkAnswer = event => {
+        if (event.target.textContent === correct) {
+            alert('Correct answer!');
+        } else {
+            alert('Wrong answer!');
+        }
+    };
+
     return <div className={classes.flashcardExercise}>
         <div className={classes['flashcardExercise__question']}>
             {question}
             </div>
-        <div className={classes['flashcardExercise__answer']}>
+        <div className={classes['flashcardExercise__answer']} onClick={checkAnswer}>
             {answers[0]}
         </div>
-        <div className={classes['flashcardExercise__answer']}>
+        <div className={classes['flashcardExercise__answer']} onClick={checkAnswer}>
             {answers[1]}
         </div>
     </div>
