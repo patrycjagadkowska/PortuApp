f13b40ee4bd89893195c813803ce201f0b248c9e
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-05-09 14:14:50 +0100

    add input validation with useRef

--- src/components/Lessons/Conjugation/Sentence.js
+++ src/components/Lessons/Conjugation/Sentence.js
@@ -1,11 +1,22 @@
+import { useRef } from 'react';
+
 import classes from './styles/Sentence.module.css';
 
 const Sentence = (props) => {
     const { sentence, correct } = props;
+    const inputRef = useRef();
+
+    const dividedSentence = sentence.split('$');
+
+    const checkAnswer = () => {
+        if(inputRef.current.value === correct) {
+            alert('Good answer!');
+        }
+    };
 
     return (
         <li className={classes.sentence}>
-            {sentence}
+            {dividedSentence[0]}<input type="text" ref={inputRef} onChange={checkAnswer} />{dividedSentence[1]}
         </li>
     );
 };
