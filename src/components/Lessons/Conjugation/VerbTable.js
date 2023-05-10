4b81803d7e2d214840765ad8890d84fdcfaf6281
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-05-10 11:38:07 +0100

    refactor verbtable (table instead of div) + basic styles

--- src/components/Lessons/Conjugation/VerbTable.js
+++ src/components/Lessons/Conjugation/VerbTable.js
@@ -4,16 +4,38 @@ const VerbTable = (props) => {
     const { verb, table } = props;
 
     return (
-      <div className={classes.table}>
-        <div className={classes["table__title"]}>{verb}</div>
-        <div className={classes["table__cell"]}>Eu {table.eu}</div>
-        <div className={classes["table__cell"]}>Tu {table.tu}</div>
-        <div className={classes["table__cell"]}>Ele/ela/você {table.voce}</div>
-        <div className={classes["table__cell"]}>Nós {table.nos}</div>
-        <div className={classes["table__cell"]}>
-          Eles/elas/vocês {table.voces}
-        </div>
-      </div>
+      <table className={classes.table}>
+        <tr>
+          <th className={classes["table__title"]} colSpan={2}>
+            {verb}
+          </th>
+        </tr>
+        <tr>
+          <td className={classes["table__cell"]} rowSpan={2}>
+            Eu <span>{table.eu}</span>
+          </td>
+          <td className={classes["table__cell"]} rowSpan={3}>
+            Nós <span>{table.nos}</span>
+          </td>
+        </tr>
+        <tr />
+        <tr>
+          <td className={classes["table__cell"]} rowSpan={2}>
+            Tu <span>{table.tu}</span>
+          </td>
+        </tr>
+        <tr>
+          <td className={classes["table__cell"]} rowSpan={3}>
+            Eles/elas/vocês <span>{table.voces}</span>
+          </td>
+        </tr>
+        <tr>
+          <td className={classes["table__cell"]} rowSpan={2}>
+            Ele/ela/você <span>{table.voce}</span>
+          </td>
+        </tr>
+        <tr />
+      </table>
     );
 };
 
