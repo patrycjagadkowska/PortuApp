08cd1d1ddd3ae0459564a72e9ec7e8bf5008eaa6
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-05-10 12:59:22 +0100

    add validation logic to connect

--- src/components/Lessons/Connect/Connect.js
+++ src/components/Lessons/Connect/Connect.js
@@ -1,16 +1,55 @@
+import { useState, useEffect } from 'react';
+
 import WordsList from './WordsList';
 
 import classes from './styles/Connect.module.css';
 
 const Connect = props => {
     const { pt, eng, title } = props.data;
+    const [ ptShuffled, setPtShuffled ] = useState([]);
+    const [ engShuffled, setEngShuffled ] = useState([]);
+    const [ foundPairs, setFoundPairs ] = useState([]);
+    const [ firstClicked, setFirstClicked ] = useState();
+    const [ firstLang, setFirstLang ] = useState();
+
+    const shuffleArray = (array) => {
+        return array.sort(() => Math.random() - 0.5);
+    };
+
+    useEffect(() => {
+        setEngShuffled(shuffleArray(eng));
+        setPtShuffled(shuffleArray(pt));
+    }, []);
+
+   const checkClickedWord = (id, lang) => {
+        if(foundPairs.includes(id)) {
+            return;
+        }
+        if(!firstClicked) {
+            setFirstClicked(id);
+            setFirstLang(lang);
+        }
+        if(firstClicked && firstClicked !== id && firstLang === lang) {
+            setFirstClicked(id);
+        }
+        if(firstClicked && firstLang !== lang && firstClicked === id) {
+            const copiedArray = [...foundPairs];
+            copiedArray.push(id);
+            setFoundPairs(copiedArray);
+            setFirstLang(undefined);
+            setFirstClicked(undefined);
+        }
+        if(firstClicked && firstLang !== lang && firstClicked !== id) {
+            alert("Wrong answer!");
+        }
+   }
 
     return (
         <div className={classes.connect}>
             <h3>{title}</h3>
             <div className={classes["connect__lists"]}>
-                <WordsList list={pt} lang="pt" />
-                <WordsList list={eng} lang="eng" />
+                <WordsList list={ptShuffled} lang="pt" checkClickedWord={checkClickedWord} />
+                <WordsList list={engShuffled} lang="eng" checkClickedWord={checkClickedWord} />
             </div>
         </div>
     );
--- src/components/Lessons/Connect/WordsList.js
+++ src/components/Lessons/Connect/WordsList.js
@@ -3,13 +3,13 @@ import Word from './Word';
 import classes from './styles/WordsList.module.css';
 
 const WordsList = props => {
-    const { list, lang } = props;
+    const { list, lang, checkClickedWord } = props;
 
-    const shuffleArray = (array) => {
-        return array.sort(() => Math.random - 0.5);
+    const checkAnswer = (id) => {
+        checkClickedWord(id, lang);
     };
 
-    const wordsList = shuffleArray(list.map(word => <Word checkAnswer={() => {}} word={word} key={`${lang}/${word.id}`} />));
+    const wordsList = list.map(word => <Word checkAnswer={checkAnswer} word={word} key={`${lang}/${word.id}`} />);
 
     return (
         <ul className={classes.wordsList}>
