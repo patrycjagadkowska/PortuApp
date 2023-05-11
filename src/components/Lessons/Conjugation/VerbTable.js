b21212433f4e776854785ff671d851d1f2131b20
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-05-11 15:03:29 +0100

    add thead and tbody tags to table

--- src/components/Lessons/Conjugation/VerbTable.js
+++ src/components/Lessons/Conjugation/VerbTable.js
@@ -5,36 +5,40 @@ const VerbTable = (props) => {
 
     return (
       <table className={classes.table}>
-        <tr>
-          <th className={classes["table__title"]} colSpan={2}>
-            {verb}
-          </th>
-        </tr>
-        <tr>
-          <td className={classes["table__cell"]} rowSpan={2}>
-            Eu <span>{table.eu}</span>
-          </td>
-          <td className={classes["table__cell"]} rowSpan={3}>
-            Nós <span>{table.nos}</span>
-          </td>
-        </tr>
-        <tr />
-        <tr>
-          <td className={classes["table__cell"]} rowSpan={2}>
-            Tu <span>{table.tu}</span>
-          </td>
-        </tr>
-        <tr>
-          <td className={classes["table__cell"]} rowSpan={3}>
-            Eles/elas/vocês <span>{table.voces}</span>
-          </td>
-        </tr>
-        <tr>
-          <td className={classes["table__cell"]} rowSpan={2}>
-            Ele/ela/você <span>{table.voce}</span>
-          </td>
-        </tr>
-        <tr />
+        <thead>
+          <tr>
+            <th className={classes["table__title"]} colSpan={2}>
+              {verb}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={classes["table__cell"]} rowSpan={2}>
+              Eu <span>{table.eu}</span>
+            </td>
+            <td className={classes["table__cell"]} rowSpan={3}>
+              Nós <span>{table.nos}</span>
+            </td>
+          </tr>
+          <tr />
+          <tr>
+            <td className={classes["table__cell"]} rowSpan={2}>
+              Tu <span>{table.tu}</span>
+            </td>
+          </tr>
+          <tr>
+            <td className={classes["table__cell"]} rowSpan={3}>
+              Eles/elas/vocês <span>{table.voces}</span>
+            </td>
+          </tr>
+          <tr>
+            <td className={classes["table__cell"]} rowSpan={2}>
+              Ele/ela/você <span>{table.voce}</span>
+            </td>
+          </tr>
+          <tr />
+        </tbody>
       </table>
     );
 };
