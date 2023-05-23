1469c69776ecbfac090e30fe065307bf365df125
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-05-23 13:05:48 +0100

    add badge icon and text node with styles

--- src/components/Learn/Lessons.js
+++ src/components/Learn/Lessons.js
@@ -1,27 +1,45 @@
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { TbArrowBadgeRight } from 'react-icons/tb';
+import { HiOutlineBadgeCheck } from 'react-icons/hi';
+
+import UserProgressContext from '../../context/UserProgressContext';
 
 import classes from './styles/Lessons.module.css';
 
 const Lessons = (props) => {
     const { lessons, unitId } = props;
     const [ lessonsList, setLessonsList] = useState();
+    const { progressData } = useContext(UserProgressContext);
     
     useEffect(() => {
         const lessonsArr = [];
         lessons.forEach(lesson => {
-            const lessonLink = 
-                (<Link to={`${unitId}/${lesson.id}`} key={`${unitId}/${lesson.id}`}>
-                    <li className={classes['lesson']}>
-                    <span className={classes['unit__item--title']}>{lesson.title}</span>
-                    <span className={classes['unit__item--badge']}>badge</span>
+            const lessonLink = (
+              <Link
+                to={`${unitId}/${lesson.id}`}
+                key={`${unitId}/${lesson.id}`}
+              >
+                <li className={classes["lesson"]}>
+                  <span className={classes["unit__item--title"]}>
+                    {lesson.title}
+                  </span>
+                  <span className={classes["unit__item--badge"]}>
+                    {progressData &&
+                    progressData[unitId] &&
+                    progressData[unitId][lesson.id] ? (
+                    <>Completed! <HiOutlineBadgeCheck className={classes.done} /></>
+                    ) : (
+                    <>Go to lesson <TbArrowBadgeRight className={classes.todo} /></>
+                    )}
+                  </span>
                 </li>
-                </Link>);
+              </Link>
+            );
             lessonsArr.push(lessonLink);
         });
         setLessonsList(lessonsArr);
-    }, [lessons, unitId]);
+    }, [lessons, unitId, progressData]);
     return (
         <ul className={classes.lessons}>
             {lessonsList}
