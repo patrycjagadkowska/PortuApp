85d660038d1d05faeaef286954d3f916b9c8ec57
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-06-09 11:33:52 +0100

    create submit button

---
+++ src/components/UI/SubmitButton.js
@@ -0,0 +1,16 @@
+import classes from './styles/SubmitButton.module.css';
+
+const SubmitButton = ({onClick, setClass, className, children}) => {
+    return (
+      <button
+        onClick={onClick}
+        className={`${classes.submitBtn} ${setClass ? classes[setClass] : ""} ${
+          className ? className : ""
+        }`}
+      >
+        {children}
+      </button>
+    );
+};
+
+export default SubmitButton;
