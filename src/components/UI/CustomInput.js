dcc8aa04ebd48f1449399e6ea676b156d8e967d9
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-06-09 11:35:34 +0100

    add onFocus prop to custom input

--- src/components/UI/CustomInput.js
+++ src/components/UI/CustomInput.js
@@ -1,10 +1,10 @@
 import classes from './styles/CustomInput.module.css';
 
-const CustomInput = ({ label, type, error, id, value, onChange }) => {
+const CustomInput = ({ label, type, error, id, value, onChange, onFocus }) => {
     return (
       <div className={classes["auth-input"]}>
         <label htmlFor={id}>{label}</label>
-        <input type={type} id={id} value={value} onChange={onChange} />
+        <input type={type} id={id} value={value} onChange={onChange} onFocus={onFocus} />
         <p className={classes["error-message"]}>{error}</p>
       </div>
     );
