ee76101a8213e7767e08bdb29a20f3d0f17439cb
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-06-12 10:44:04 +0100

    add basic styles to question and answer

--- src/components/Test/Question.js
+++ src/components/Test/Question.js
@@ -5,7 +5,7 @@ import classes from './styles/Question.module.css';
 const Question = () => {
     return (
         <div className={classes.question}>
-            <h3>Here is question</h3>
+            <header className={classes["question__header"]}><h3>Here is question</h3></header>
             <div className={classes["question__answers"]}>
                 <Answer />
                 <Answer />
