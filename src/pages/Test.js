855e4ace86d3a6ffacb86071e2d6e543aa52402e
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-06-13 10:55:21 +0100

    style Test page

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -22,14 +22,16 @@ const Test = () => {
         <h2>Test Unit {unitNum}</h2>
         {!quizStarted && (
           <>
-            <p>
+            <p className={classes["test__rules"]}>
               You will have to answer to 10 questions. For each question you
               have 4 possible answers. To answer for each question you have 10
               seconds. To pass this test you have to answer correctly to at
               least 8 of them. Good luck!
             </p>
-            <CustomButton onClick={handleStartTest}>start</CustomButton>
-            <ButtonNavLink to="/learn">go back</ButtonNavLink>
+            <div className={classes["test__actions"]}>
+              <CustomButton onClick={handleStartTest}>start</CustomButton>
+              <ButtonNavLink to="/learn">go back</ButtonNavLink>
+            </div>
           </>
         )}
         {quizStarted && <TestContainer />}
