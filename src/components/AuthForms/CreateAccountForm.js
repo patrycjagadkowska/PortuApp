11ebf103a9cda875f4921b5398cd2efc25f82d68
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-06-28 11:18:32 +0100

    format code in auth forms

--- src/components/AuthForms/CreateAccountForm.js
+++ src/components/AuthForms/CreateAccountForm.js
@@ -1,139 +1,142 @@
-import { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { BsPerson } from 'react-icons/bs';
-import { RiLockPasswordLine } from 'react-icons/ri';
-import { BiError } from 'react-icons/bi';
-import { useNavigate } from 'react-router';
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { BsPerson } from "react-icons/bs";
+import { RiLockPasswordLine } from "react-icons/ri";
+import { BiError } from "react-icons/bi";
+import { useNavigate } from "react-router";
 
-import { auth } from '../../api/auth-api';
-import CustomInput from '../UI/CustomInput';
-import { useInput } from '../../hooks/useInput';
-import SubmitButton from '../UI/SubmitButton';
+import { auth } from "../../api/auth-api";
+import CustomInput from "../UI/CustomInput";
+import { useInput } from "../../hooks/useInput";
+import SubmitButton from "../UI/SubmitButton";
 
-import classes from './styles/AuthForm.module.css';
+import classes from "./styles/AuthForm.module.css";
 
 const CreateAccountForm = () => {
-    const email = useInput("");
-    const password = useInput("");
-    const repeatedPassword = useInput("");
-    const [ emailError, setEmailError ] = useState();
-    const [ passError, setPassError ] = useState();
-    const [ repeatedPassError, setRepeatedPassError ] = useState();
-    const navigate = useNavigate();
-    const [ submitButtonClass, setSubmitButtonClass ] = useState("");
+  const email = useInput("");
+  const password = useInput("");
+  const repeatedPassword = useInput("");
+  const [emailError, setEmailError] = useState();
+  const [passError, setPassError] = useState();
+  const [repeatedPassError, setRepeatedPassError] = useState();
+  const navigate = useNavigate();
+  const [submitButtonClass, setSubmitButtonClass] = useState("");
 
-    const submitHandler = (event) => {
-        event.preventDefault();
+  const submitHandler = (event) => {
+    event.preventDefault();
 
-        setEmailError();
-        setPassError();
-        setRepeatedPassError();
+    setEmailError();
+    setPassError();
+    setRepeatedPassError();
 
-        setSubmitButtonClass("spin");
+    setSubmitButtonClass("spin");
 
-        if (email.value.length === 0) {
-            setSubmitButtonClass("error");
-            setEmailError("Please enter your email");
-            return;
-        } else if (password.value.length === 0) {
-            setSubmitButtonClass("error");
-            setPassError("Please enter you password");
-            return;
-        } else if (repeatedPassword.value.length === 0) {
-          setSubmitButtonClass("error")
-          setRepeatedPassError("Please repeat your password");
-            return;
-        } else if (password.value !== repeatedPassword.value) {
-          setSubmitButtonClass("error");
-          setRepeatedPassError("Passwords are not equal");
-            return;
+    if (email.value.length === 0) {
+      setSubmitButtonClass("error");
+      setEmailError("Please enter your email");
+      return;
+    } else if (password.value.length === 0) {
+      setSubmitButtonClass("error");
+      setPassError("Please enter you password");
+      return;
+    } else if (repeatedPassword.value.length === 0) {
+      setSubmitButtonClass("error");
+      setRepeatedPassError("Please repeat your password");
+      return;
+    } else if (password.value !== repeatedPassword.value) {
+      setSubmitButtonClass("error");
+      setRepeatedPassError("Passwords are not equal");
+      return;
+    }
+
+    createUserWithEmailAndPassword(auth, email.value, password.value)
+      .then((userCredential) => {
+        setSubmitButtonClass("success");
+        setTimeout(() => {
+          navigate("/login");
+        }, 1000);
+      })
+      .catch((error) => {
+        setSubmitButtonClass("error");
+        if (error.code === "auth/email-already-in-use") {
+          setRepeatedPassError("This email is already registered");
+        } else if (error.code === "auth/network-request-failed") {
+          setRepeatedPassError("Network error occured");
+        } else if (error.code === "auth/too-many-requests") {
+          setRepeatedPassError(
+            "We're sorry, we got too many requests. Please try again later."
+          );
+        } else {
+          setRepeatedPassError(error.message);
         }
+      });
+  };
 
-        createUserWithEmailAndPassword(auth, email.value, password.value)
-            .then((userCredential) => {
-              setSubmitButtonClass("success");
-              setTimeout(() => {
-                navigate("/login");
-              }, 1000);
-            }).catch(error => {
-              setSubmitButtonClass("error");
-              if (error.code === "auth/email-already-in-use") {
-                setRepeatedPassError("This email is already registered");
-              } else if (error.code === "auth/network-request-failed") {
-                setRepeatedPassError("Network error occured");
-              } else if (error.code === "auth/too-many-requests") {
-                setRepeatedPassError("We're sorry, we got too many requests. Please try again later.");
-              } else {
-                setRepeatedPassError(error.message);
-              }
-            });
-    };
-
-    return (
-      <form className={classes.form}>
-        <CustomInput
-          value={email.value}
-          onChange={email.onChange}
-          label={
+  return (
+    <form className={classes.form}>
+      <CustomInput
+        value={email.value}
+        onChange={email.onChange}
+        label={
+          <>
+            <BsPerson /> Email
+          </>
+        }
+        type="email"
+        id="email"
+        error={
+          emailError && (
             <>
-              <BsPerson /> Email
+              <BiError /> {emailError}
             </>
-          }
-          type="email"
-          id="email"
-          error={
-            emailError && (
-              <>
-                <BiError /> {emailError}
-              </>
-            )
-          }
-        />
-        <CustomInput
-          value={password.value}
-          onChange={password.onChange}
-          label={
+          )
+        }
+      />
+      <CustomInput
+        value={password.value}
+        onChange={password.onChange}
+        label={
+          <>
+            <RiLockPasswordLine /> Password
+          </>
+        }
+        type="password"
+        id="password"
+        error={
+          passError && (
             <>
-              <RiLockPasswordLine /> Password
+              <BiError /> {passError}
             </>
-          }
-          type="password"
-          id="password"
-          error={
-            passError && (
-              <>
-                <BiError /> {passError}
-              </>
-            )
-          }
-        />
-        <CustomInput
-          value={repeatedPassword.value}
-          onChange={repeatedPassword.onChange}
-          label={
+          )
+        }
+      />
+      <CustomInput
+        value={repeatedPassword.value}
+        onChange={repeatedPassword.onChange}
+        label={
+          <>
+            <RiLockPasswordLine /> Repeat password
+          </>
+        }
+        type="password"
+        id="repeated-password"
+        error={
+          repeatedPassError && (
             <>
-              <RiLockPasswordLine /> Repeat password
+              <BiError /> {repeatedPassError}
             </>
-          }
-          type="password"
-          id="repeated-password"
-          error={
-            repeatedPassError && (
-              <>
-                <BiError /> {repeatedPassError}
-              </>
-            )
-          }
-        />
-        <SubmitButton
-          onClick={submitHandler}
-          className={classes["create-account-button"]}
-          setClass={submitButtonClass}
-        >
-          Create account
-        </SubmitButton>
-      </form>
-    );
+          )
+        }
+      />
+      <SubmitButton
+        onClick={submitHandler}
+        className={classes["create-account-button"]}
+        setClass={submitButtonClass}
+      >
+        Create account
+      </SubmitButton>
+    </form>
+  );
 };
 
 export default CreateAccountForm;
--- src/components/AuthForms/LoginForm.js
+++ src/components/AuthForms/LoginForm.js
@@ -1,109 +1,110 @@
-import { useContext, useState } from 'react';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { useNavigate } from 'react-router';
-import { BsPerson } from 'react-icons/bs';
-import { BiError } from 'react-icons/bi';
-import { RiLockPasswordLine } from 'react-icons/ri';
+import { useContext, useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router";
+import { BsPerson } from "react-icons/bs";
+import { BiError } from "react-icons/bi";
+import { RiLockPasswordLine } from "react-icons/ri";
 
-import { auth } from '../../api/auth-api';
-import AuthContext from '../../context/AuthContext';
-import { useInput } from '../../hooks/useInput';
-import CustomInput from '../UI/CustomInput';
-import SubmitButton from '../UI/SubmitButton';
+import { auth } from "../../api/auth-api";
+import AuthContext from "../../context/AuthContext";
+import { useInput } from "../../hooks/useInput";
+import CustomInput from "../UI/CustomInput";
+import SubmitButton from "../UI/SubmitButton";
 
-import classes from './styles/AuthForm.module.css';
+import classes from "./styles/AuthForm.module.css";
 
 const LoginForm = () => {
-    const email = useInput("");
-    const password = useInput("");
-    const authCtx = useContext(AuthContext);
-    const navigate = useNavigate();
-    const [ emailError, setEmailError ] = useState();
-    const [ passwordError, setPasswordError ] = useState();
-    const [ submitButtonClass, setSubmitButtonClass ] = useState("");
+  const email = useInput("");
+  const password = useInput("");
+  const authCtx = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [emailError, setEmailError] = useState();
+  const [passwordError, setPasswordError] = useState();
+  const [submitButtonClass, setSubmitButtonClass] = useState("");
 
-    const submitHandler = (event) => {
-        event.preventDefault();
-        setEmailError();
-        setPasswordError();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    setEmailError();
+    setPasswordError();
 
-        setSubmitButtonClass("spin");
+    setSubmitButtonClass("spin");
 
-        if (email.value.length === 0) {
-          setSubmitButtonClass("error");
-          setEmailError("Please enter your email.");
-          return;
-        } else if (password.value.length === 0) {
-          setSubmitButtonClass("error");
-          setEmailError("Please enter your password.");
-          return;
+    if (email.value.length === 0) {
+      setSubmitButtonClass("error");
+      setEmailError("Please enter your email.");
+      return;
+    } else if (password.value.length === 0) {
+      setSubmitButtonClass("error");
+      setEmailError("Please enter your password.");
+      return;
+    }
+
+    signInWithEmailAndPassword(auth, email.value, password.value)
+      .then((userCredential) => {
+        setSubmitButtonClass("success");
+        const user = userCredential.user;
+        authCtx.login(user.uid);
+        navigate("/learn");
+      })
+      .catch((error) => {
+        setSubmitButtonClass("error");
+        if (error.code === "auth/wrong-password") {
+          setPasswordError("Password is incorrect.");
+        } else if (error.code === "auth/invalid-email") {
+          setPasswordError("Email is incorrect or not registered yet.");
+        } else if (error.code === "auth/user-disabled") {
+          setPasswordError(
+            "Your account has been disabled. Please contact us for more information"
+          );
+        } else {
+          setPasswordError(error.message);
         }
+      });
+  };
 
-        signInWithEmailAndPassword(auth, email.value, password.value)
-            .then((userCredential) => {
-              setSubmitButtonClass("success");
-                const user = userCredential.user;
-                authCtx.login(user.uid);
-                navigate('/learn');
-            }).catch((error) => {
-              setSubmitButtonClass("error");
-              if (error.code === "auth/wrong-password") {
-                setPasswordError("Password is incorrect.");
-              } else if (error.code === "auth/invalid-email") {
-                setPasswordError("Email is incorrect or not registered yet.");
-              } else if (error.code === "auth/user-disabled") {
-                setPasswordError(
-                  "Your account has been disabled. Please contact us for more information"
-                );
-              } else {
-                setPasswordError(error.message);
-              }
-            });
-    };
-    
-    return (
-      <form className={classes.form}>
-        <CustomInput
-          type="email"
-          value={email.value}
-          onChange={email.onChange}
-          label={
+  return (
+    <form className={classes.form}>
+      <CustomInput
+        type="email"
+        value={email.value}
+        onChange={email.onChange}
+        label={
+          <>
+            <BsPerson /> Email
+          </>
+        }
+        id="email"
+        error={
+          emailError && (
             <>
-              <BsPerson /> Email
+              <BiError /> {emailError}
             </>
-          }
-          id="email"
-          error={
-            emailError && (
-              <>
-                <BiError /> {emailError}
-              </>
-            )
-          }
-        />
-        <CustomInput
-          type="password"
-          value={password.value}
-          onChange={password.onChange}
-          label={
+          )
+        }
+      />
+      <CustomInput
+        type="password"
+        value={password.value}
+        onChange={password.onChange}
+        label={
+          <>
+            <RiLockPasswordLine /> Password
+          </>
+        }
+        id="password"
+        error={
+          passwordError && (
             <>
-              <RiLockPasswordLine /> Password
+              <BiError /> {passwordError}
             </>
-          }
-          id="password"
-          error={
-            passwordError && (
-              <>
-                <BiError /> {passwordError}
-              </>
-            )
-          }
-        />
-        <SubmitButton onClick={submitHandler} setClass={submitButtonClass}>
-          login
-        </SubmitButton>
-      </form>
-    );
+          )
+        }
+      />
+      <SubmitButton onClick={submitHandler} setClass={submitButtonClass}>
+        login
+      </SubmitButton>
+    </form>
+  );
 };
 
 export default LoginForm;
