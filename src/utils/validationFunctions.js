3cea4859b80336902fc6821f4a938f2331aa5a7a
Patrycja Gadkowska <patrycjagadkowska@gmail.com>
2023-06-28 11:40:06 +0100

    format code in validationFunctions.js

--- src/utils/validationFunctions.js
+++ src/utils/validationFunctions.js
@@ -1,6 +1,6 @@
 export const verifyEmail = (email) => {
-    const emailRegex = /^[A-Z0-9._-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    return emailRegex.test(email);
+  const emailRegex = /^[A-Z0-9._-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+  return emailRegex.test(email);
 };
 
 export const verifyName = (name) => {
